0512b1298a9ee68169d193db2836ea0096b2bbff
Sonia Seuc <sonia.seuc@gmail.com>
2020-09-22 11:26:25 -0500

    edit ok & authent guard also ok, autorization guard still not ready

--- src/app/Guards/auth-guards.service.ts
+++ src/app/Guards/auth-guards.service.ts
@@ -1,26 +1,24 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { CanActivate } from '@angular/router';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 import {  AuthConfigService } from '../../app/services/auth-config.service';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 
 @Injectable()
 export class AuthGuardsService implements CanActivate {
-
+currentUser = this.authConfigService.currentUser;
   constructor(private authConfigService: AuthConfigService, private router: Router) { }
 
   canActivate(): Observable<boolean> {
-    return this.authConfigService.currentUser.pipe(
-      map(user => {
-        if (!user) {
-          this.router.navigate(['/home']);
-          console.log(false);
-          return false;
-        }
-        return true;
-      })
-    );
+    const isAuth = this.authConfigService.isAuth();
+    console.log("is Auth", isAuth);  
+    return of (isAuth) // "of" lo crea observable
+    .pipe(tap( result=> {  // "tap" no devuelve algo, no altera info observable
+      if (!result) {
+        this.router.navigate(['/login']);
+      }
+    }) )
   }
   }
 
